b76e939635091b7a695a436f4bef691814f460f9
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-11-15 22:50:59 -0300

    realice las validaciones en las rutas de usuarios, productos y pedidos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,15 +1,128 @@
-import { Router } from "express";
-import { borrarProducto, buscarProducto, editarProducto, guardarProducto, listarProductos } from "../controllers/productos.controller";
+import { Router } from 'express';
+import { check } from 'express-validator';
+import {
+  borrarProducto,
+  buscarProducto,
+  editarProducto,
+  guardarProducto,
+  listarProductos,
+} from '../controllers/productos.controller';
 
-const router = Router();
+const router = Router()
 
-router.route("/productos")
-.get(listarProductos)
-.post(guardarProducto)
+router
+  .route('/productos')
+  .get(listarProductos)
+  .post(
+    [
+      check('nombre')
+        .notEmpty()
+        .withMessage('El nombre es un dato obligatorio')
+        .isString()
+        .withMessage('El dato ingresado debe ser de tipo string')
+        .isLength({
+          min: 2,
+          max: 150,
+        })
+        .withMessage(
+          'El nombre del usuario puede tener entre 2 y 150 caracteres',
+        ),
+      check('estado')
+        .notEmpty()
+        .withMessage('El estado es un dato obligatorio')
+        .isBoolean()
+        .withMessage('El dato ingresado debe ser un booleano'),
+      check('precio')
+        .notEmpty()
+        .withMessage('El precio es un valor obligatorio')
+        .isNumeric()
+        .withMessage('El valor ingresado debe ser numerico')
+        .custom((value) => {
+          if (value >= 1 && value <= 7000) {
+            return true
+          } else {
+            throw new Error('El precio debe estar entre 1 y 7000')
+          }
+        }),
+      check('detalle')
+        .notEmpty()
+        .withMessage('El detalle es un dato obligatorio')
+        .isString()
+        .withMessage('El detalle debe ser de tipo string')
+        .isLength({
+          min: 10,
+          max: 200,
+        })
+        .withMessage('El detalle debe tener entre 10 y 200 caracteres'),
+      check('categoria')
+        .notEmpty()
+        .withMessage('La categoria es un campo Obligatorio')
+        .isIn(['Para Compartir', 'Tacos', 'Brochetas', 'Sushi', 'Tazones'])
+        .withMessage('Debe ingresar una categoria valida'),
+      check('imagen')
+        .notEmpty('La URL del producto es obligatoria')
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage('Debe ingresar una URL valida'),
+    ],
+    guardarProducto,
+  )
 
-router.route("/productos/:id")
-.get(buscarProducto)
-.put(editarProducto)
-.delete(borrarProducto)
+router
+  .route('/productos/:id')
+  .get(buscarProducto)
+  .put(
+    [
+      check('nombre')
+        .notEmpty()
+        .withMessage('El nombre es un dato obligatorio')
+        .isString()
+        .withMessage('El dato ingresado debe ser de tipo string')
+        .isLength({
+          min: 2,
+          max: 150,
+        })
+        .withMessage(
+          'El nombre del usuario puede tener entre 2 y 150 caracteres',
+        ),
+      check('estado')
+        .notEmpty()
+        .withMessage('El estado es un dato obligatorio')
+        .isBoolean()
+        .withMessage('El dato ingresado debe ser un booleano'),
+      check('precio')
+        .notEmpty()
+        .withMessage('El precio es un valor obligatorio')
+        .isNumeric();
+        .withMessage('El valor ingresado debe ser numerico')
+        .custom((value) => {
+          if (value >= 1 && value <= 7000) {
+            return true
+          } else {
+            throw new Error('El precio debe estar entre 1 y 7000')
+          }
+        }),
+      check('detalle')
+        .notEmpty()
+        .withMessage('El detalle es un dato obligatorio')
+        .isString()
+        .withMessage('El detalle debe ser de tipo string')
+        .isLength({
+          min: 10,
+          max: 200,
+        })
+        .withMessage('El detalle debe tener entre 10 y 200 caracteres'),
+      check('categoria')
+        .notEmpty()
+        .withMessage('La categoria es un campo Obligatorio')
+        .isIn(['Para Compartir', 'Tacos', 'Brochetas', 'Sushi', 'Tazones'])
+        .withMessage('Debe ingresar una categoria valida'),
+      check('imagen')
+        .notEmpty('La URL del producto es obligatoria')
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage('Debe ingresar una URL valida'),
+    ],
+    editarProducto,
+  )
+  .delete(borrarProducto)
 
-export default router;
+export default router
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,15 +1,102 @@
-import { Router } from "express";
-import { borrarUsuario, buscarUsuario, editarUsuario, guardarUsuario, listarUsuarios } from "../controllers/usuarios.controller";
+import { Router } from 'express';
+import { check } from 'express-validator';
+import {
+  borrarUsuario,
+  buscarUsuario,
+  editarUsuario,
+  guardarUsuario,
+  listarUsuarios,
+} from '../controllers/usuarios.controller';
 
-const router = Router();
+const router = Router()
 
-router.route("/usuarios")
-.get(listarUsuarios)
-.post(guardarUsuario);
+router
+  .route('/usuarios')
+  .get(listarUsuarios)
+  .post(
+    [
+      check('nombre')
+        .notEmpty()
+        .withMessage('El nombre es un dato obligatorio')
+        .isString()
+        .withMessage('El dato ingresado debe ser de tipo string')
+        .isLength({
+          min: 2,
+          max: 40,
+        })
+        .withMessage(
+          'El nombre del usuario puede tener entre 2 y 40 caracteres',
+        ),
+      check('email')
+        .notEmpty()
+        .withMessage('El email es un dato obligatorio')
+        .isEmail()
+        .withMessage('El email es un dato incorrecto'),
+      check('password')
+        .notEmpty()
+        .withMessage('La contraseña es un dato obligatorio')
+        .isLength({
+          min: 6,
+          max: 20,
+        })
+        .withMessage(
+          'La contraseña debe tener como minimo 6 caracteres y maximo 20',
+        ),
+      check('perfil')
+        .notEmpty('')
+        .withMessage('El perfil es un dato obligatorio'),
+      check('estado')
+        .notEmpty()
+        .withMessage('El estado es un dato obligatorio')
+        .isBoolean()
+        .withMessage('El estado debe ser un valor booleano'),
+    ],
+    guardarUsuario,
+  )
 
-router.route("/usuarios/:id")
-.get(buscarUsuario)
-.put(editarUsuario)
-.delete(borrarUsuario);
+router
+  .route('/usuarios/:id')
+  .get(buscarUsuario)
+  .put(
+    [
+      check('nombre')
+        .notEmpty()
+        .withMessage('El nombre es un dato obligatorio')
+        .isString()
+        .withMessage('El dato ingresado debe ser de tipo string')
+        .isLength({
+          min: 2,
+          max: 40,
+        })
+        .withMessage(
+          'El nombre del usuario puede tener entre 2 y 40 caracteres',
+        ),
+      check('email')
+        .notEmpty()
+        .withMessage('El email es un dato obligatorio')
+        .isEmail()
+        .withMessage('El email es un dato incorrecto'),
+      check('password')
+        .notEmpty()
+        .withMessage('La contraseña es un dato obligatorio')
+        .isLength({
+          min: 6,
+          max: 20,
+        })
+        .withMessage(
+          'La contraseña debe tener como minimo 6 caracteres y maximo 20',
+        ),
+      check('perfil')
+        .notEmpty('')
+        .withMessage('El perfil es un dato obligatorio'),
+      check('estado')
+        .notEmpty()
+        .withMessage('El estado es un dato obligatorio')
+        .isBoolean()
+        .withMessage('El estado debe ser un valor booleano'),
+    ],
+    editarUsuario,
+  )
+  .delete(borrarUsuario)
 
-export default router;
+export default router
