e629193a23c1e3986c44c910ddf1ab2de56a1e1a
Agustin Sampayo <agustines82@gmail.com>
2022-11-25 19:51:59 -0300

    modifico en pedido js el tipo de dato de la fecha a ingresar

--- src/models/pedido.js
+++ src/models/pedido.js
@@ -6,7 +6,7 @@ const pedidoSchema = new Schema({
         required: true,
     },
     fecha: {
-        type: Date,
+        type: String,
         required: true,
     },
     productos: {
--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,127 +1,113 @@
-import { check } from 'express-validator';
-const { Router } = require('express');
-const {
-  listarPedidos,
-  guardarPedido,
-  buscarPedido,
-  editarPedido,
-  borrarPedido,
-} = require('../controllers/pedidos.controllers');
+import { check } from "express-validator";
+const { Router } = require("express");
+const { listarPedidos, guardarPedido, buscarPedido, editarPedido, borrarPedido } = require("../controllers/pedidos.controllers");
 
-const router = Router()
+const router = Router();
 
 router
-  .route('/pedidos')
-  .get(listarPedidos)
-  .post(
-    [
-      check('usuario')
-        .notEmpty()
-        .withMessage('El usuario debe ser un dato obligatorio')
-        .isString()
-        .withMessage('El dato ingresado debe ser de tipo string')
-        .isLength({
-          min: 2,
-          max: 40,
-        })
-        .withMessage('El usuario debe estar entre 2 y 40 caracteres'),
-      check('fecha')
-        .notEmpty()
-        .withMessage('La fecha es un dato obligatorio')
-        .isDate()
-        .withMessage('La fecha debe ser de tipo date'),
-      check('productos')
-        .notEmpty()
-        .withMessage('Los productos son un dato obligatorio')
-        .isArray({
-          min: 1,
-          max: 15,
-        })
-        .withMessage('El pedido debe contener entre 1 y 10 productos'),
-      check('domicilio')
-        .notEmpty()
-        .withMessage('El domicilio es un dato obligatorio')
-        .isString()
-        .withMessage('El domicilio debe ser de tipo string')
-        .isLength({
-          min: 5,
-          max: 100,
-        })
-        .withMessage('El domicilio debe estar entre 5 y 100 caracteres'),
-      check('indicaciones')
-        .notEmpty()
-        .withMessage('Las indicaciones son un dato obligatorio')
-        .isString()
-        .withMessage('Las indicaciones debe ser de tipo string')
-        .isLength({
-          min: 5,
-          max: 100,
-        })
-        .withMessage('Las indicaciones debe estar entre 5 y 100 caracteres'),
-      check('estado')
-        .notEmpty()
-        .withMessage('El estado es un dato obligatorio')
-        .isBoolean()
-        .withMessage('El dato ingresado debe ser un booleano'),
-    ],
-    guardarPedido,
-  )
+    .route("/pedidos")
+    .get(listarPedidos)
+    .post(
+        [
+            check("usuario")
+                .notEmpty()
+                .withMessage("El usuario debe ser un dato obligatorio")
+                .isString()
+                .withMessage("El dato ingresado debe ser de tipo string")
+                .isLength({
+                    min: 2,
+                    max: 40,
+                })
+                .withMessage("El usuario debe estar entre 2 y 40 caracteres"),
+            check("fecha").notEmpty().withMessage("La fecha es un dato obligatorio").isString().withMessage("La fecha debe ser de tipo date"),
+            check("productos")
+                .notEmpty()
+                .withMessage("Los productos son un dato obligatorio")
+                .isArray({
+                    min: 1,
+                    max: 15,
+                })
+                .withMessage("El pedido debe contener entre 1 y 10 productos"),
+            check("domicilio")
+                .notEmpty()
+                .withMessage("El domicilio es un dato obligatorio")
+                .isString()
+                .withMessage("El domicilio debe ser de tipo string")
+                .isLength({
+                    min: 5,
+                    max: 100,
+                })
+                .withMessage("El domicilio debe estar entre 5 y 100 caracteres"),
+            check("indicaciones")
+                .notEmpty()
+                .withMessage("Las indicaciones son un dato obligatorio")
+                .isString()
+                .withMessage("Las indicaciones debe ser de tipo string")
+                .isLength({
+                    min: 5,
+                    max: 100,
+                })
+                .withMessage("Las indicaciones debe estar entre 5 y 100 caracteres"),
+            check("estado")
+                .notEmpty()
+                .withMessage("El estado es un dato obligatorio")
+                .isBoolean()
+                .withMessage("El dato ingresado debe ser un booleano"),
+        ],
+        guardarPedido
+    );
 router
-  .route('/pedidos/:id')
-  .get(buscarPedido)
-  .put(
-    [
-      check('usuario')
-        .notEmpty()
-        .withMessage('El usuario debe ser un dato obligatorio')
-        .isString()
-        .withMessage('El dato ingresado debe ser de tipo string')
-        .isLength({
-          min: 2,
-          max: 40,
-        })
-        .withMessage('El usuario debe estar entre 2 y 40 caracteres'),
-      check('fecha')
-        .notEmpty()
-        .withMessage('La fecha es un dato obligatorio')
-        .isDate()
-        .withMessage('La fecha debe ser de tipo date'),
-      check('productos')
-        .notEmpty()
-        .withMessage('Los productos son un dato obligatorio')
-        .isArray({
-          min: 1,
-          max: 15,
-        })
-        .withMessage('El pedido debe contener entre 1 y 10 productos'),
-      check('domicilio')
-        .notEmpty()
-        .withMessage('El domicilio es un dato obligatorio')
-        .isString()
-        .withMessage('El domicilio debe ser de tipo string')
-        .isLength({
-          min: 5,
-          max: 100,
-        })
-        .withMessage('El domicilio debe estar entre 5 y 100 caracteres'),
-      check('indicaciones')
-        .notEmpty()
-        .withMessage('Las indicaciones son un dato obligatorio')
-        .isString()
-        .withMessage('Las indicaciones debe ser de tipo string')
-        .isLength({
-          min: 5,
-          max: 100,
-        })
-        .withMessage('Las indicaciones debe estar entre 5 y 100 caracteres'),
-      check('estado')
-        .notEmpty()
-        .withMessage('El estado es un dato obligatorio')
-        .isBoolean()
-        .withMessage('El dato ingresado debe ser un booleano'),
-    ],
-    editarPedido,
-  )
-  .delete(borrarPedido)
+    .route("/pedidos/:id")
+    .get(buscarPedido)
+    .put(
+        [
+            check("usuario")
+                .notEmpty()
+                .withMessage("El usuario debe ser un dato obligatorio")
+                .isString()
+                .withMessage("El dato ingresado debe ser de tipo string")
+                .isLength({
+                    min: 2,
+                    max: 40,
+                })
+                .withMessage("El usuario debe estar entre 2 y 40 caracteres"),
+            check("fecha").notEmpty().withMessage("La fecha es un dato obligatorio").isDate().withMessage("La fecha debe ser de tipo date"),
+            check("productos")
+                .notEmpty()
+                .withMessage("Los productos son un dato obligatorio")
+                .isArray({
+                    min: 1,
+                    max: 15,
+                })
+                .withMessage("El pedido debe contener entre 1 y 10 productos"),
+            check("domicilio")
+                .notEmpty()
+                .withMessage("El domicilio es un dato obligatorio")
+                .isString()
+                .withMessage("El domicilio debe ser de tipo string")
+                .isLength({
+                    min: 5,
+                    max: 100,
+                })
+                .withMessage("El domicilio debe estar entre 5 y 100 caracteres"),
+            check("indicaciones")
+                .notEmpty()
+                .withMessage("Las indicaciones son un dato obligatorio")
+                .isString()
+                .withMessage("Las indicaciones debe ser de tipo string")
+                .isLength({
+                    min: 5,
+                    max: 100,
+                })
+                .withMessage("Las indicaciones debe estar entre 5 y 100 caracteres"),
+            check("estado")
+                .notEmpty()
+                .withMessage("El estado es un dato obligatorio")
+                .isBoolean()
+                .withMessage("El dato ingresado debe ser un booleano"),
+        ],
+        editarPedido
+    )
+    .delete(borrarPedido);
 
-export default router
+export default router;
